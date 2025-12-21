74b92b9b9a62ac4ef5b59183680ec0867de581dd
andreashgk <andreas@hgk.me>
2025-12-21 01:49:16 +0100

    refactor: improve cli output and add more flags

--- z-image-cli/src/main.rs
+++ z-image-cli/src/main.rs
@@ -1,12 +1,20 @@
 #![recursion_limit = "256"]
 
-use std::{path::PathBuf, process::ExitCode};
+use std::{path::PathBuf, process::ExitCode, time::Duration};
 
+use burn::{
+    Tensor,
+    prelude::Backend,
+    store::{ModuleStore, SafetensorsStore},
+    tensor::{DType, Distribution},
+    vision::utils::{ColorDisplayOpts, ImageDimOrder, TensorDisplayOptions, save_tensor_as_image},
+};
 use clap::Parser;
-use rootcause::prelude::ResultExt;
+use indicatif::{ProgressBar, ProgressStyle};
+use rootcause::{Report, prelude::ResultExt, report};
 use z_image::{
-    GenerateOpts,
     modules::{ae::AutoEncoderConfig, transformer::ZImageModelConfig},
+    scheduler::{FlowMatchEulerDiscreteScheduler, Shift},
 };
 
 cfg_if::cfg_if! {
@@ -45,6 +53,15 @@ struct Args {
     /// The height of the image in pixels.
     #[arg(short = 'H', long, default_value_t = 1024)]
     height: usize,
+    /// The amount of sampling steps.
+    #[arg(long, default_value_t = 8)]
+    steps: u32,
+    /// How many images to generate at once.
+    #[arg(long, default_value_t = 1)]
+    batch_size: usize,
+    /// Whether to use dynamic shift in the scheduler.
+    #[arg(long, action)]
+    dynamic_shift: bool,
 }
 
 fn main() -> ExitCode {
@@ -55,43 +72,134 @@ fn main() -> ExitCode {
     #[cfg(feature = "tch")]
     let device = burn::backend::libtorch::LibTorchDevice::Cuda(0);
 
-    let mut autoencoder = AutoEncoderConfig::flux_ae().init(&device);
-    println!("Loading autoencoder");
-    if let Err(err) = autoencoder
-        .load_weights(args.ae)
-        .context("Failed to load autoencoder weights")
-    {
-        eprintln!("{err}");
-        return ExitCode::FAILURE;
-    }
-
-    let mut transformer = ZImageModelConfig::default().init::<B>(&device);
-    println!("Loading transformer");
-    if let Err(err) = transformer
-        .load_weights(args.transformer)
-        .context("Failed to load transformer weights")
-    {
-        eprintln!("{err}");
-        return ExitCode::FAILURE;
-    }
-
-    println!("Generating image");
-    let result = z_image::generate::<B>(
-        &GenerateOpts {
-            prompt_path: args.prompt_file,
-            out_path: args.out,
-            width: args.width,
-            height: args.height,
-        },
-        &autoencoder,
-        &transformer,
-        &device,
-    );
+    let result = generate::<B>(args, &device);
     if let Err(err) = result {
         eprintln!("{err}");
         return ExitCode::FAILURE;
     }
 
-    println!("Done");
     ExitCode::SUCCESS
+}
+
+fn generate<B: Backend>(args: Args, device: &B::Device) -> Result<(), Report> {
+    let batch_size = 1;
+    let num_inference_steps = args.steps;
+    let width = args.width;
+    let height = args.height;
+
+    let mut prompt_store = SafetensorsStore::from_file(&args.prompt_file);
+    let prompt_data = prompt_store
+        .get_snapshot("prompt")
+        .context("Failed to read prompt file")?
+        .ok_or_else(|| report!("Missing key `prompt` in prompt file"))?
+        .to_data()
+        .context("Failed to load prompt from prompt file")?;
+    let prompt_embedding = Tensor::<B, 2>::from_data(prompt_data, device).unsqueeze_dim::<3>(0);
+
+    let vae_scale_factor = 8;
+    let vae_scale = vae_scale_factor * 2;
+
+    if width % vae_scale != 0 || height % vae_scale != 0 {
+        return Err(
+            report!("Width and height must be a multiple of {vae_scale}")
+                .attach(format!("width: {width}"))
+                .attach(format!("height: {height}")),
+        );
+    }
+
+    let latent_width = 2 * (width / vae_scale);
+    let latent_height = 2 * (height / vae_scale);
+
+    let latents_shape = [
+        batch_size,
+        16, // = in_channels
+        latent_height,
+        latent_width,
+    ];
+    let latents = Tensor::<B, 4>::random(latents_shape, Distribution::Normal(0., 1.), device);
+
+    let scheduler = FlowMatchEulerDiscreteScheduler::<B>::new(
+        num_inference_steps,
+        1000,
+        match args.dynamic_shift {
+            true => Shift::dynamic(
+                (latents_shape[2] / 2) * (latents_shape[3] / 2),
+                Default::default(),
+            ),
+            false => Shift::Constant(3.0),
+        },
+        device,
+    );
+
+    let pb = spinner("Loading model");
+    let transformer = ZImageModelConfig::default()
+        .init::<B>(device)
+        .with_weights(args.transformer)
+        .context("Failed to load transformer weights")?;
+    pb.finish();
+
+    let pb = ProgressBar::new(num_inference_steps as u64);
+    pb.set_style(
+        ProgressStyle::with_template(
+            "{spinner:.blue.bold} {msg} {wide_bar:.blue/white.dim.dim} {pos}/{len} • {per_sec} • {eta}/{elapsed}",
+        )
+        .unwrap()
+        .tick_strings(&[" ⠋", " ⠙", " ⠹", " ⠸", " ⠼", " ⠴", " ⠦", " ⠧", " ⠇", " ⠏", "OK"])
+        .progress_chars("━╸━"),
+    );
+    pb.set_message("Sampling");
+    let mut sampler = scheduler.sampler(&transformer, latents, prompt_embedding);
+    for (i, _) in (&mut sampler).enumerate() {
+        pb.set_position((i + 1) as u64);
+    }
+    pb.finish();
+    let latents = sampler.result();
+
+    drop(transformer);
+    B::memory_cleanup(&device);
+
+    let pb = spinner("Loading autoencoder");
+    let autoencoder = AutoEncoderConfig::flux_ae()
+        .init(device)
+        .with_weights(args.ae)
+        .context("Failed to load autoencoder weights")?;
+    pb.finish();
+
+    let pb = spinner("Decoding image");
+    let image = autoencoder.decode(latents);
+    pb.finish();
+
+    drop(autoencoder);
+
+    let pb = spinner("Saving image");
+    save_tensor_as_image(
+        image.cast(DType::F32),
+        TensorDisplayOptions {
+            dim_order: ImageDimOrder::Nchw,
+            color_opts: ColorDisplayOpts::Rgb,
+            batch_opts: None,
+            width_out: width,
+            height_out: height,
+        },
+        &args.out,
+    )
+    .map_err(|err| report!(err.to_string()))
+    .context("Failed to save image")
+    .attach_with(|| format!("destination path: {}", &args.out.to_string_lossy()))?;
+    pb.finish();
+    Ok(())
+}
+
+fn spinner(msg: &'static str) -> ProgressBar {
+    let pb = ProgressBar::new_spinner();
+    pb.set_style(
+        ProgressStyle::with_template("{spinner:.blue.bold} {msg}")
+            .unwrap()
+            .tick_strings(&[
+                " ⠋", " ⠙", " ⠹", " ⠸", " ⠼", " ⠴", " ⠦", " ⠧", " ⠇", " ⠏", "OK",
+            ]),
+    );
+    pb.enable_steady_tick(Duration::from_millis(120));
+    pb.set_message(msg);
+    pb
 }
